8f9c3901d79dab13871177e41584b8dbac89c9cd
viniciussnitram <vinicius.martins.vsm@outlook.com>
2024-04-06 15:34:28 -0300

    feat: add a tab navigation. code when the user clicks on an animal picture, shows a description of this animal

---
+++ script.js
@@ -0,0 +1,19 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('active');
+
+    function activeTab(index) {
+        tabContent.forEach(content => {
+            content.classList.remove('active');
+        });
+        tabContent[index].classList.add('active');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            activeTab(index);
+        });
+    });
+}
