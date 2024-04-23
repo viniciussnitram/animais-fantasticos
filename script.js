48fe077dcee834c4480afa768deebf5659812418
viniciussnitram <vinicius.martins.vsm@outlook.com>
2024-04-23 18:15:14 -0300

    feat: added a function to add the 'scroll animation' to the page, the content appears slowly

--- script.js
+++ script.js
@@ -63,3 +63,28 @@ function initSmoothScroll() {
 }
 
 initSmoothScroll();
+
+function initScrollAnimation() {
+    const sections = document.querySelectorAll('.js-scroll');
+    
+    if (sections.length) {  
+        const halfWindow = window.innerHeight * 0.6;
+        
+        function animateScroll() {
+            sections.forEach(section => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - halfWindow) < 0;
+                if (isSectionVisible)
+                    section.classList.add('active');
+                else
+                    section.classList.remove('active');
+            });
+        }
+        
+        animateScroll();
+        
+        window.addEventListener('scroll', animateScroll);
+    }
+}
+
+initScrollAnimation();
