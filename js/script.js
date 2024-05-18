1ce41f152e204cadf148eaadd7365b88ef253314
viniciussnitram <vinicius.martins.vsm@outlook.com>
2024-05-18 12:07:11 -0300

    feat: import the js code modal in the script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initTabNav from './module/tabnav.js';
 import initAccordion from './module/accordion.js';
 import initSmoothScroll from './module/smooth-scroll.js';
 import initScrollAnimation from './module/scroll-animation.js';
+import initModal from './module/modal.js';
 
 initTabNav();
 initAccordion();
 initSmoothScroll();
 initScrollAnimation();
+initModal();
