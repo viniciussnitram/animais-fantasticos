34a2ee75bb7fd36c3fbb5e61bc4a53f6baec5607
viniciussnitram <vinicius.martins.vsm@outlook.com>
2024-05-18 17:58:20 -0300

    feat: manipulated the tooltip with js. using mousemove, mouseleave and mouseover to change the state of the tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAccordion from './module/accordion.js';
 import initSmoothScroll from './module/smooth-scroll.js';
 import initScrollAnimation from './module/scroll-animation.js';
 import initModal from './module/modal.js';
+import initTooltip from './module/tooltip.js';
 
 initTabNav();
 initAccordion();
 initSmoothScroll();
 initScrollAnimation();
 initModal();
+initTooltip();
