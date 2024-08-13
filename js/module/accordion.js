81ef3403b9dc99c5e556611e368e004e5eb808bf
viniciussnitram <vinicius.martins.vsm@outlook.com>
2024-08-13 19:57:55 -0300

    fix: fixed eslint and prettier to work together

--- js/module/accordion.js
+++ js/module/accordion.js
@@ -1,18 +1,20 @@
 export default function initAccordion() {
-    const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-    const activeClass = 'active';
+  const accordionList = document.querySelectorAll(
+    '[data-anime="accordion"] dt'
+  );
+  const activeClass = "active";
 
-    if (accordionList.length) {
-        accordionList[0].classList.add(activeClass);
-        accordionList[0].nextElementSibling.classList.add(activeClass);
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
 
-        function activeAccordion() {
-            this.classList.toggle(activeClass);
-            this.nextElementSibling.classList.toggle(activeClass);
-        }
-
-        accordionList.forEach(item => {
-            item.addEventListener('click', activeAccordion);
-        });
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
     }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
 }
